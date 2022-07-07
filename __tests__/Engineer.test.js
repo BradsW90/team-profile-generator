adf88ff282ac8068ab1393a67354f4bc8c1ff54c
Amanda Hefner <bswj90@gmail.com>
2022-07-07 01:34:05 -0400

    finished setting up engineer test and js file

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,15 @@
+const Engineer = require("../lib/Engineer");
+
+jest.mock("../lib/Engineer");
+
+describe("Engineer", () => {
+  it("Should return Engineer object", () => {
+    const result = new Engineer();
+
+    expect(result.getName()).toEqual("Engineer");
+    expect(result.getId()).toEqual("1224");
+    expect(result.getEmail()).toEqual("engineer@gmail.com");
+    expect(result.getUserName()).toEqual("github.com/engineer");
+    expect(result.getRole()).toEqual("Engineer");
+  });
+});
