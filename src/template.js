22f0687f9b427213441e268515f5a9a64468d831
Amanda Hefner <bswj90@gmail.com>
2022-07-10 19:19:54 -0400

    finished touch up on template

--- src/template.js
+++ src/template.js
@@ -13,52 +13,52 @@ const rosterPage = (rosterData) => {
   return ` <!DOCTYPE html>
   <html lang=en>
   <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Team Roster</title>
-  <script src="https://kit.fontawesome.com/5bae9e23d6.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="./style.css" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Team Roster</title>
+    <script src="https://kit.fontawesome.com/5bae9e23d6.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./style.css" />
   </head>
   <body>
-  <h1>My Team</h1>
-  <div class="card-container">
-  <div class="card">
-  <div class="card-title">
-  <h3>${newManager.getName()}</h3>
-  <h3><i class="fa-solid fa-person">${newManager.getRole()}</h3>
-  </div>
-  <div class="attribute">ID: ${newManager.getId()}</div>
-  <div class="attribute">Email: <a href="mailTo:${newManager.getEmail()}">${newManager.getEmail()}</a></div>
-  <div class="attribute">Office number: ${newManager.getPhoneNumber()}</div>
-  </div>
-  ${rosterData.roster
-    .map(function (classes) {
-      if (classes.getRole() === "Intern") {
-        return `<div class="card">
-    <div class="card-title">
-    <h3>${classes.getName()}</h3>
-    <h3><i class="fa-solid fa-school"><i>${classes.getRole()}</h3>
-    </div>
-    <div class="attribute">ID: ${classes.getId()}</div>
-    <div class="attribute">Email: <a href="mailTo:${classes.getEmail()}">${classes.getEmail()}</a></div>
-    <div class="attribute">School Attending: ${classes.getSchool()}</div>
-    </div>`;
-      } else {
-        return `<div class="card">
-      <div class="card-title">
-      <h3>${classes.getName()}</h3>
-      <h3><i class="fa-brands fa-dev"></i>${classes.getRole()}</h3>
+    <h1>My Team</h1>
+    <div class="card-container">
+      <div class="card">
+        <div class="card-title">
+          <h3>${newManager.getName()}</h3>
+          <h3><i class="fa-solid fa-person"></i>${newManager.getRole()}</h3>
+        </div>
+        <div class="attribute">ID: ${newManager.getId()}</div>
+        <div class="attribute">Email: <a href="mailTo:${newManager.getEmail()}">${newManager.getEmail()}</a></div>
+        <div class="attribute">Office number: <a href="tel:${newManager.getPhoneNumber()}">${newManager.getPhoneNumber()}</a></div>
       </div>
-      <div class="attribute">ID: ${classes.getId()}</div>
-      <div class="attribute">Email: <a href="mailTo:${
-        classes.getEmail
-      }">${classes.getEmail()}</a></div>
-      <div class="attribute">GitHub account: <a href="github.com/${classes.getGithub()}">${classes.getGithub()}</a></div>
-      </div>`;
-      }
-    })
-    .join("")}
-  </div>
+      ${rosterData.roster
+        .map(function (classes) {
+          if (classes.getRole() === "Intern") {
+            return `<div class="card">
+        <div class="card-title">
+          <h3>${classes.getName()}</h3>
+          <h3><i class="fa-solid fa-school"></i>${classes.getRole()}</h3>
+        </div>
+        <div class="attribute">ID: ${classes.getId()}</div>
+        <div class="attribute">Email: <a href="mailTo:${classes.getEmail()}">${classes.getEmail()}</a></div>
+        <div class="attribute">School Attending: ${classes.getSchool()}</div>
+        </div>`;
+          } else {
+            return `<div class="card">
+          <div class="card-title">
+            <h3>${classes.getName()}</h3>
+            <h3><i class="fa-brands fa-dev"></i>${classes.getRole()}</h3>
+          </div>
+          <div class="attribute">ID: ${classes.getId()}</div>
+          <div class="attribute">Email: <a href="mailTo:${
+            classes.getEmail
+          }">${classes.getEmail()}</a></div>
+          <div class="attribute">GitHub account: <a href="github.com/${classes.getGithub()}">${classes.getGithub()}</a></div>
+          </div>`;
+          }
+        })
+        .join("")}
+      </div>
   </body>
   </html>`;
 };
